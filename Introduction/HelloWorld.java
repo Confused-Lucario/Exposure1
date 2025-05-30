b2cf8c13f34df72abcc1d1f33e416e13bcc4271a
The Cat <philip.birkland@vtsd.com>
2025-05-30 11:35:05 -0400

    day 2
    100 points

---
+++ Introduction/HelloWorld.java
@@ -0,0 +1,11 @@
+package Introduction;
+
+// create class HelloWorld
+public class HelloWorld {
+
+    // Create Main Method
+    public static void main(String[] args) {
+        // Output to the window hello world
+        System.out.println("Hello World");
+    }
+}
